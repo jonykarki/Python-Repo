ead556d9ade8e15bb68dba3bcf68e7b2f92279a1
Jony Karki <johnnykarki0@gmail.com>
2017-10-08 16:04:54 +0545

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,51 +1,27 @@
-# scraper for hamrobazar.com
-# /*//*///**////**/**/
-
+# Scraper, daraz.com.np
+# version 0.1 /beta/
 
 import bs4 as bs
 import urllib.request
 
 
-# Item the user wants to find
-searchTerm = input("What are you looking for?   ")
-print(searchTerm)
+BASE_URL = "https://www.daraz.com.np/catalog/?q="
 
-# find the results of the searchTerm in hamrobazar
-sauce = urllib.request.urlopen('http://hamrobazaar.com/search.php?do_search=Search&searchword=' + searchTerm + '&Search.x=0&Search.y=0&catid_search=0').read()
 
+user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+
+headers={'User-Agent':user_agent,} 
+
+searchTerm = input('What are you looking for?   ')
+
+url = BASE_URL + searchTerm
+request = urllib.request.Request(url, None, headers)
+response = urllib.request.urlopen(request)
+sauce = response.read()
 
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-# print all the HTML
-# print(soup)
+data = soup.find_all("span", class_="name")
 
-# find the table of datas
-table = soup.table
-
-# let's just find the table inside of the li tag
-li_tag = soup.li
-
-# find the table inside of the li
-for table in li_tag:
-    table_rows = table.find_all('tr')
-
-    for tr in table_rows:
-        td= tr.find_all('td')
-        for i in td:
-            row_data = i.text
-        print(row_data)
-
-
-# table_rows = table.find_all('tr')
-
-# get the data from the table rows
-##for tr in table_rows:
-##    td = tr.find_all('td')
-##    for i in td:
-##        row = i.text
-##    print(row)
-
-    
-# print(table)
-
+print(data)
 
