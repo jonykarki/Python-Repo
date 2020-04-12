2a65b9253a6a53a98cb670795f468867adf529ab
Jony Karki <25265687+jonykarki@users.noreply.github.com>
2020-04-12 16:06:35 -0500

    Update EchoBot.py

--- EchoBot.py
+++ EchoBot.py
@@ -1,18 +1,17 @@
-#!/usr/local/bin/python 
+from fbchat import log, Client
 
-from fbchat import Client
-
-EMAIL = "ouremail@email.com"
-PASSWORD = "secret"
-
+# Subclass fbchat.Client and override required methods
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(author_id, thread_id)
-        self.markAsRead(author_id)
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
 
-        # don't reply on our own messages
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If you're not the author, echo
         if author_id != self.uid:
             self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
-client = EchoBot(USERNAME, PASSWORD)
+
+client = EchoBot("<email>", "<password>")
 client.listen()
